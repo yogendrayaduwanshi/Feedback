edfe2de7419880d4b7d182f964851d6e4bfd0fa8
yogendrayaduwanshi <yogendrayaduwanshi19@gmail.com>
2016-06-12 23:21:47 +0530

    Wi_Fi Activity

--- MainActivity.java
+++ MainActivity.java
@@ -1,105 +1,93 @@
-package com.example.yogendra19.excel;
+package com.example.yogendra19.wi_fi;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WifiManager wifiManager;
+    private BroadcastReceiver wifireciever;
+    private ArrayAdapter adapter;
+    Switch btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void order (View v) {
-        HSSFWorkbook workbook = new HSSFWorkbook();
-        HSSFSheet sheet = workbook.createSheet("FirstExcelSheet");
-        HSSFRow row = sheet.createRow(0);
-        HSSFCell cell = row.createCell(0);
-        cell.setCellValue("Name");
-        /*cell = row.createCell(1);
-        DataFormat format = workbook.createDataFormat();
-        CellStyle dateStyle = workbook.createCellStyle();
-        dateStyle.setDataFormat(format.getFormat("dd.mm.yyyy"));
-        cell.setCellStyle(dateStyle);
-        cell.setCellValue(new Date());*/
-        row.createCell(1).setCellValue("Surname");
-        /*sheet.autoSizeColumn(1);*/
+        ListView listview = (ListView) findViewById(R.id.listView);
+        adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_2);
+        listview.setAdapter(adapter);
+        wifiManager = (WifiManager)this.getSystemService(Context.WIFI_SERVICE);
+        wifireciever = new WiFiScanReceiver();
 
-        try {
-            workbook.write(new FileOutputStream("/storage/emulated/0/Android/excel.xls"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-        public void view(View v){
+        btn = (Switch) findViewById(R.id.switch1);
+        btn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
-            HSSFWorkbook workbook = null;
-            String value = null;
-            String value1 = null;
-            try {
-                workbook = new HSSFWorkbook(new FileInputStream("/storage/emulated/0/Android/excel.xls"));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            HSSFSheet sheet = workbook.getSheetAt(0);
-            HSSFRow row = sheet.getRow(0);
-            value = row.getCell(0).getStringCellValue();
-            value1 = row.getCell(1).getStringCellValue();
-           /* if (row.getCell(0).getCellType() == HSSFCell.CELL_TYPE_STRING) {
-                 value=row.getCell(0).getStringCellValue();
-            }
-            if (row.getCell(1).getCellType() == HSSFCell.CELL_TYPE_NUMERIC) {
-                System.out.println(row.getCell(1).getDateCellValue());
-            }*/
-            TextView x=(TextView)findViewById(R.id.textView);
-            TextView y=(TextView)findViewById(R.id.textView2);
-            x.setText(value);
-            y.setText(value1);
-    }
-  }
- /*   public void order(View v) {
-        try {
+                if (isChecked) {
+                    toggleWiFi(true);
+                    Toast.makeText(getApplicationContext(), "Wi-Fi Enabled!", Toast.LENGTH_LONG).show();
 
-            AssetManager am=getAssets();// If this line gives you ERROR then try AssetManager am=getActivity().getAssets();
-            InputStream is=am.open("excel.xls");
-            Workbook wb =Workbook.getWorkbook(is);
-            Sheet s=wb.getSheet(0);
-            int row =s.getRows();
-            int col=s.getColumns();
-            String xx="";
-            for(int i=0;i<row;i++)
-            {
-                for (int c=0;c<col;c++)
-                {
-                    Cell z=s.getCell(c,i);
-                    xx=xx+z.getContents();
-
+                } else {
+                    toggleWiFi(false);
+                    Toast.makeText(getApplicationContext(), "Wi-Fi Disabled!", Toast.LENGTH_LONG).show();
                 }
-                xx=xx+"\n";
             }
-            display(xx);
-        }
-
-        catch (Exception e)
-        {
-
-        }
-
+        });
     }
-    public void display(String value)
-    {
-        TextView x=(TextView)findViewById(R.id.textView);
-        x.setText(value);
-    }*/
+
+    public void toggleWiFi(boolean status){
+       // WifiManager wifiManager = (WifiManager)this.getSystemService(WIFI_SERVICE);
+        if (status && !wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(true);
+        } else if (!status && wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(false);
+        }
+    }
+
+    class WiFiScanReceiver extends BroadcastReceiver {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (WifiManager.SCAN_RESULTS_AVAILABLE_ACTION.equals(action)) {
+                List<ScanResult> wifiScanResultList = wifiManager.getScanResults();
+                for(int i = 0; i < wifiScanResultList.size(); i++){
+                    ScanResult accessPoint = wifiScanResultList.get(i);
+                    String listItem = accessPoint.SSID+", "+accessPoint.BSSID+", "+accessPoint.capabilities;
+                    adapter.add(listItem);
+                }
+            }
+        }
+    }
+
+
+    protected void onResume() {
+        super.onResume();
+        // Register the BroadcastReceiver for SCAN_RESULTS_AVAILABLE_ACTION
+        IntentFilter filter = new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        registerReceiver(wifireciever, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(wifireciever);
+    }
+
+
+
+
+
+}
